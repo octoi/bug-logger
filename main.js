e154f0221bb815eea1f4e53ac8ef37cd731d0f89
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-16 21:34:09 +0530

    delete log send

--- main.js
+++ main.js
@@ -94,6 +94,16 @@ ipcMain.on('logs:add', async (e, item) => {
     }
 })
 
+// delete log
+ipcMain.on('logs:delete', async (e, id) => {
+    try {
+        await Log.findOneAndDelete({ _id: id })
+        sendLogs();
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
