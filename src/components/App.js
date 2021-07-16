71f1091c52be80e6c9881809a6b00d7619102003
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-16 21:26:40 +0530

    removed boiler data

--- src/components/App.js
+++ src/components/App.js
@@ -7,29 +7,7 @@ import LogItem from './LogItem'
 import AddLogItem from './AddLogItem'
 
 const App = () => {
-    const [logs, setLogs] = useState([
-        {
-            _id: 1,
-            text: 'This is log one',
-            priority: 'low',
-            user: 'Brad',
-            created: new Date().toString(),
-        },
-        {
-            _id: 2,
-            text: 'This is log two',
-            priority: 'moderate',
-            user: 'Kate',
-            created: new Date().toString(),
-        },
-        {
-            _id: 3,
-            text: 'This is log three',
-            priority: 'high',
-            user: 'John',
-            created: new Date().toString(),
-        },
-    ])
+    const [logs, setLogs] = useState([])
     const [alert, setAlert] = useState({
         show: false,
         message: '',
