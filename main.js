d338d59b479085555d1e0bd5ce0b33af6bb476ac
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-16 20:45:59 +0530

    changed tab sapce

--- main.js
+++ main.js
@@ -7,76 +7,76 @@ let mainWindow
 let isDev = false
 
 if (
-  process.env.NODE_ENV !== undefined &&
-  process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV !== undefined &&
+    process.env.NODE_ENV === 'development'
 ) {
-  isDev = true
+    isDev = true
 }
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    width: isDev ? 1400 : 1100,
-    height: 800,
-    show: false,
-    backgroundColor: 'white',
-    icon: `${__dirname}/assets/icons/icon.png`,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  })
-
-  let indexPath
-
-  if (isDev && process.argv.indexOf('--noDevServer') === -1) {
-    indexPath = url.format({
-      protocol: 'http:',
-      host: 'localhost:8080',
-      pathname: 'index.html',
-      slashes: true,
+    mainWindow = new BrowserWindow({
+        width: isDev ? 1400 : 1100,
+        height: 800,
+        show: false,
+        backgroundColor: 'white',
+        icon: `${__dirname}/assets/icons/icon.png`,
+        webPreferences: {
+            nodeIntegration: true,
+        },
     })
-  } else {
-    indexPath = url.format({
-      protocol: 'file:',
-      pathname: path.join(__dirname, 'dist', 'index.html'),
-      slashes: true,
-    })
-  }
 
-  mainWindow.loadURL(indexPath)
+    let indexPath
 
-  // Don't show until we are ready and loaded
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-
-    // Open devtools if dev
-    if (isDev) {
-      const {
-        default: installExtension,
-        REACT_DEVELOPER_TOOLS,
-      } = require('electron-devtools-installer')
-
-      installExtension(REACT_DEVELOPER_TOOLS).catch((err) =>
-        console.log('Error loading React DevTools: ', err)
-      )
-      mainWindow.webContents.openDevTools()
+    if (isDev && process.argv.indexOf('--noDevServer') === -1) {
+        indexPath = url.format({
+            protocol: 'http:',
+            host: 'localhost:8080',
+            pathname: 'index.html',
+            slashes: true,
+        })
+    } else {
+        indexPath = url.format({
+            protocol: 'file:',
+            pathname: path.join(__dirname, 'dist', 'index.html'),
+            slashes: true,
+        })
     }
-  })
 
-  mainWindow.on('closed', () => (mainWindow = null))
+    mainWindow.loadURL(indexPath)
+
+    // Don't show until we are ready and loaded
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+
+        // Open devtools if dev
+        if (isDev) {
+            const {
+                default: installExtension,
+                REACT_DEVELOPER_TOOLS,
+            } = require('electron-devtools-installer')
+
+            installExtension(REACT_DEVELOPER_TOOLS).catch((err) =>
+                console.log('Error loading React DevTools: ', err)
+            )
+            mainWindow.webContents.openDevTools()
+        }
+    })
+
+    mainWindow.on('closed', () => (mainWindow = null))
 }
 
 app.on('ready', createMainWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createMainWindow()
-  }
+    if (mainWindow === null) {
+        createMainWindow()
+    }
 })
 
 // Stop error
--- src/components/LogItem.js
+++ src/components/LogItem.js
@@ -4,38 +4,38 @@ import Button from 'react-bootstrap/Button'
 import Badge from 'react-bootstrap/Badge'
 
 const LogItem = ({
-  deleteItem,
-  log: { _id, priority, user, text, created },
+    deleteItem,
+    log: { _id, priority, user, text, created },
 }) => {
-  const setVariant = () => {
-    if (priority === 'high') {
-      return 'danger'
-    } else if (priority === 'moderate') {
-      return 'warning'
-    } else {
-      return 'success'
+    const setVariant = () => {
+        if (priority === 'high') {
+            return 'danger'
+        } else if (priority === 'moderate') {
+            return 'warning'
+        } else {
+            return 'success'
+        }
     }
-  }
 
-  return (
-    <tr>
-      <td>
-        <Badge variant={setVariant()} className='p-2'>
-          {priority.charAt(0).toUpperCase() + priority.slice(1)}
-        </Badge>
-      </td>
-      <td>{text}</td>
-      <td>{user}</td>
-      <td>
-        <Moment format='MMMM Do YYYY, h:mm:ss a'>{new Date(created)}</Moment>
-      </td>
-      <td>
-        <Button variant='danger' size='sm' onClick={() => deleteItem(_id)}>
-          x
-        </Button>
-      </td>
-    </tr>
-  )
+    return (
+        <tr>
+            <td>
+                <Badge variant={setVariant()} className='p-2'>
+                    {priority.charAt(0).toUpperCase() + priority.slice(1)}
+                </Badge>
+            </td>
+            <td>{text}</td>
+            <td>{user}</td>
+            <td>
+                <Moment format='MMMM Do YYYY, h:mm:ss a'>{new Date(created)}</Moment>
+            </td>
+            <td>
+                <Button variant='danger' size='sm' onClick={() => deleteItem(_id)}>
+                    x
+                </Button>
+            </td>
+        </tr>
+    )
 }
 
 export default LogItem
